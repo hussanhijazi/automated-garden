21dca58ebbdd0c6da89ac0dc209f646946e2b663
Houssan Ali Hijazi <hussanhijazi@gmail.com>
2022-09-26 14:01:19 -0300

    Added send notification method to firebase

--- backend/firebase.py
+++ backend/firebase.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import messaging
 
 databaseURL = 'https://automated-garden-d403e-default-rtdb.firebaseio.com/'
 
@@ -15,3 +16,14 @@ def connect():
 def save(topic, values):
     ref = db.reference(topic)
     ref.push().set(values)
+
+
+def send_notification(registration_token, status):
+    message = messaging.Message(
+        data={'status': status},
+        token=registration_token,
+    )
+
+    response = messaging.send(message)
+
+    print('Successfully sent message:', response)
--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,13 @@ from config import topic_water
 from mqtt_client import on_connect
 from mqtt_client import on_publish
 from mqtt_client import on_subscribe
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
 
 soil_value = 0
 temp_value = 0
@@ -21,48 +28,68 @@ water_status = False
 water_threshold = 55
 
 firebase.connect()
+
 mqtt_client = paho.Client(mqtt_config['client_id'], userdata=None, protocol=paho.MQTTv5)
 mqtt_client.on_connect = on_connect
 mqtt_client.connect(mqtt_config['mqtt_broker'], mqtt_config['port'])
 
 
 def on_message(client, userdata, msg):
-    """
-        Prints a mqtt message to stdout ( used as callback for subscribe )
-
-        :param client: the client itself
-        :param userdata: userdata is set when initiating the client, here it is userdata=None
-        :param msg: the message with topic and payload
-    """
-    global soil_value, temp_value, humidity_value, rain_value, water_status
-    print(msg.topic + " - " + str(msg.qos) + " - " + str(msg.payload))
+    logging.info(msg.topic + " - " + str(msg.qos) + " - " + str(msg.payload))
     value = msg.payload.decode('ASCII')
 
-    if msg.topic == topic_soil.decode('ASCII'):
-        soil_value = float(value)
-    elif msg.topic == topic_rain.decode('ASCII'):
-        rain_value = float(value)
-    elif msg.topic == topic_temp.decode('ASCII'):
-        temp_value = float(value)
-    elif msg.topic == topic_humidity.decode('ASCII'):
-        humidity_value = float(value)
+    set_value(msg.topic, value)
 
-    topic = msg.topic.replace('/', '-')
+    save_value(msg.topic, value)
+
+    check_status()
+
+
+def check_status():
+    global water_status
+    if not water_status:
+        if soil_value > water_threshold:
+            water_status = True
+            set_status('on')
+    else:
+        if soil_value <= water_threshold:
+            water_status = False
+            set_status('off')
+
+
+def set_status(status):
+    mqtt_client.publish(topic_water.decode('ASCII'), status.encode())
+    save_status(status)
+    #firebase.send_notification('{TOKEN}', status)
+
+
+def save_status(state):
     timestamp = time.time()
+    firebase.save('water_state', {
+        'timestamp': timestamp,
+        'state': state
+    })
 
+
+def save_value(topic, value):
+    topic = topic.replace('/', '_')
+    timestamp = time.time()
     firebase.save(topic, {
         'timestamp': timestamp,
         'value': value
     })
 
-    if not water_status:
-        if soil_value > water_threshold:
-            water_status = True
-            client.publish(topic_water.decode('ASCII'), b'on')
-    else:
-        if soil_value <= water_threshold:
-            water_status = False
-            client.publish(topic_water.decode('ASCII'), b'off')
+
+def set_value(topic, value):
+    global soil_value, rain_value, temp_value, humidity_value
+    if topic == topic_soil.decode('ASCII'):
+        soil_value = float(value)
+    elif topic == topic_rain.decode('ASCII'):
+        rain_value = float(value)
+    elif topic == topic_temp.decode('ASCII'):
+        temp_value = float(value)
+    elif topic == topic_humidity.decode('ASCII'):
+        humidity_value = float(value)
 
 
 mqtt_client.on_subscribe = on_subscribe
