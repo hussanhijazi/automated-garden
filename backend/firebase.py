8739565c344edd1531dd43ed925fac1eab51b44f
Houssan Ali Hijazi <hussanhijazi@gmail.com>
2023-01-30 16:15:27 -0300

    Send push notification

--- backend/firebase.py
+++ backend/firebase.py
@@ -26,16 +26,59 @@ def save_firestore(client, collection, data):
     doc_ref.set(data)
 
 
-def send_notification(registration_token, status):
+def send_notification(client, title, msg):
+    registration_token = get_user_tokens(client)
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification(
+            title=title,
+            body=msg,
+        ),
+        tokens=registration_token,
+    )
+    response = messaging.send_multicast(message)
+
+    print('{0} messages were sent successfully'.format(response.success_count))
+
+def send_notification2(title, msg):
     message = messaging.Message(
-        data={"status": status},
-        token=registration_token,
+        notification=messaging.Notification(
+            title=title,
+            body=msg,
+        ),
+        token="csu1a2z_SVuSB5ga_s03uK:APA91bEcKE17bm8S7L0ddrXMKRFPigOta4B7lujySbyLiZpi9BZ09NENp9ZxchipLRBK2DDiTFvN9hWNtl0j5251P5mlQjirW9WSxtzoVeXliW_dm4u1sxVW9mE23c-mIFySWC6d_gPW",
     )
 
     response = messaging.send(message)
 
     print("Successfully sent message:", response)
 
+def get_user_tokens(client):
+    user_tokens_ref = client.collection("users_tokens")
+    try:
+        my_dict = [el.to_dict()["token"] for el in user_tokens_ref.get()]
+        return my_dict
+    except Exception as e:
+        print(e)
+
+def get_presets(client):
+    preset_ref = client.collection("presets").order_by("timestamp", direction=firestore.Query.DESCENDING).limit(1)
+    try:
+        my_dict = [el.to_dict() for el in preset_ref.get()]
+        return my_dict
+    except Exception as e:
+        print(e)
+
+def get_temperature(client, collection, limit):
+    temp_collection_ref = client.collection(collection)
+    try:
+        w = temp_collection_ref.order_by("timestamp", direction=firestore.Query.DESCENDING).limit(limit)
+        my_dict = [{el.id: {"value": el.to_dict()["value"]} for el in w.get()}]
+        return my_dict
+    except Exception as e:
+        print(e)
+        return "off"
+def delete_temperature(client, collection, timestamp):
+    client.collection(collection).document(str(timestamp)).delete()
 
 def actual_water_state(client):
     water_state_collection_ref = client.collection(water_state_collection)
@@ -50,18 +93,3 @@ def actual_water_state(client):
         return water_status
 
     return "off"
-
-
-def get_temperature(client, collection, limit):
-    temp_collection_ref = client.collection(collection)
-    try:
-        w = temp_collection_ref.order_by("timestamp", direction=firestore.Query.DESCENDING).limit(limit)
-        my_dict = [{el.id: {"value": el.to_dict()["value"]} for el in w.get()}]
-        return my_dict
-    except Exception as e:
-        print(e)
-        return "off"
-
-
-def delete_temperature(client, collection, timestamp):
-    client.collection(collection).document(str(timestamp)).delete()
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 import json
 import logging
-
+import time
 import paho.mqtt.client as paho
-
+import datetime
 import firebase as my_firebase
 from config import mqtt_config
 from config import topic_humidity
@@ -14,6 +14,7 @@ from config import water_state_collection
 from mqtt_client import on_connect
 from mqtt_client import on_publish
 from mqtt_client import on_subscribe
+from presets import check_time
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -21,48 +22,75 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 firestore_client = my_firebase.connect()
-actual_water_state = my_firebase.actual_water_state(firestore_client)
 soil_value = 0
 temp_value = 0
 rain_value = 0
 humidity_value = 0
-water_status = True if actual_water_state == 'on' else False
-water_threshold = 55
+#water_threshold = 55
 
 mqtt_client = paho.Client(mqtt_config['client_id'], userdata=None, protocol=paho.MQTTv5)
 mqtt_client.on_connect = on_connect
 mqtt_client.connect(mqtt_config['mqtt_broker'], mqtt_config['port'])
 
-
+water_status = False
 def on_message(client, userdata, msg):
-    logging.info(msg.topic + " - " + str(msg.qos) + " - " + str(msg.payload))
     value = msg.payload.decode()
     data = json.loads(value)
 
     set_value(msg.topic, data['value'])
+   # save_value(msg.topic, data)
 
-    save_value(msg.topic, data)
+    if msg.topic == topic_soil.decode():
+        logging.info(msg.topic + " - " + str(msg.qos) + " - " + str(msg.payload))
+        check_water_status(data['timestamp'])
+        # time.sleep(0.5)
 
-    check_status(data['timestamp'])
 
 
-def check_status(timestamp):
-    if soil_value > water_threshold:
-        send_status('on', timestamp)
-        save_status('on', timestamp)
+def check_water_status(timestamp):
+    global water_status
+    presets = my_firebase.get_presets(firestore_client)
+
+    start = presets[0]["time1"]
+    end = presets[0]["time2"]
+    soil_humidity = presets[0]["soilHumidity"]
+
+    parts_start = start.split(":")
+    parts_end = end.split(":")
+
+    time_start = datetime.time(int(parts_start[0]), int(parts_start[1]), 0)
+    time_end = datetime.time(int(parts_end[0]), int(parts_end[1]), 0)
+
+    start_seconds = check_time(time_start)
+    end_seconds = check_time(time_end)
+
+    can_start_time = -0.01 > start_seconds > -1
+    can_end_time = -0.01 > end_seconds > -1
+
+    logging.info(str(time_start) + " - Seconds Start: " + str(start_seconds) + " - " + str(can_start_time))
+    logging.info(str(time_end) + " - Seconds End: " + str(end_seconds) + " - " + str(can_end_time))
+    logging.info("water status: " + str(water_status))
+
+    if soil_value >= int(soil_humidity):
+        if water_status:
+            send_water_status('off', timestamp)
+            save_water_status('off', timestamp)
+            my_firebase.send_notification(firestore_client, "Automated Garden", "Irrigação desligada com sucesso...")
+            water_status = False
     else:
-        if soil_value <= water_threshold:
-            send_status('off', timestamp)
-            save_status('off', timestamp)
+        if (can_start_time or can_end_time) and water_status == False:
+            send_water_status('on', timestamp)
+            save_water_status('on', timestamp)
+            my_firebase.send_notification(firestore_client, "Automated Garden", "Irrigação ligada com sucesso...")
+            water_status = True
 
-
-def send_status(status, timestamp):
+def send_water_status(status, timestamp):
     mqtt_client.publish(topic_water.decode(), status.encode())
     logging.info('Water State - ' + str(status) + ' - ' + str(timestamp))
     #firebase.send_notification('{TOKEN}', status)
 
 
-def save_status(state, timestamp):
+def save_water_status(state, timestamp):
     # firebase.save('water_state', {
     #     'timestamp': timestamp,
     #     'state': state
